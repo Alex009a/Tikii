f91b00b5ad0bcc34ff261044f46b5ec1c33588f3
Alex009a <130747916+Alex009a@users.noreply.github.com>
2023-07-26 17:18:49 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./xmrig -o ironfish.unmineable.com:4444/ -a rx -k -u TRX:TH7zzkXi81hLDQQzn3P1kvxrFYXXU7Z5PK.mymine -p x
+./xmrig -o ironfish.unmineable.com:4444 -a rx -k -u TRX:TH7zzkXi81hLDQQzn3P1kvxrFYXXU7Z5PK.mymine -p x
