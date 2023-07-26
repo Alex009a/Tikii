92014cc720e68ab947dfb503ba3e3432fb8a996e
Alex009a <130747916+Alex009a@users.noreply.github.com>
2023-07-26 11:42:32 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-x64.tar.gz && tar -zxvf xmrig-6.20.0-linux-x64.tar.gz && cd xmrig-6.20.0 && clear && chmod +x xmrig
+wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-x64.tar.gz && tar -zxvf xmrig-6.20.0-linux-x64.tar.gz && cd xmrig-6.20.0 && chmod +x xmrig
