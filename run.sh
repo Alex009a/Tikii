eb8d9be188ff842c61f235c6b00a21cd36edef5a
Alex009a <130747916+Alex009a@users.noreply.github.com>
2023-08-04 11:56:00 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u ETH:0x81815a68BB36BDc1Ecc728A518770B6A3b6554DD.unmineable_worker_wwzmvls -p x
+./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u ETH:0x81815a68BB36BDc1Ecc728A518770B6A3b6554DD.unmineable_worker_wwzmvls -p x --proxy=188.93.213.242:1080
